30029ab9c8172584369bdb1e44506a7f99aeecef
gijslebesque <gijslebesque@gmail.com>
2020-12-04 15:57:40 +0100

    first commit

---
+++ custom.js
@@ -0,0 +1,35 @@
+/* 
+    DOM manipulation
+
+
+    Het doel hier is om te leren hoe je mbv JS de DOM manipuleert.
+    En hoe je documentatie leest.
+
+    DOM manipulatie vindt meestal plaats op een (user) event.
+
+    Hier is een voorbeeld.
+    https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+
+
+    - Opdracht 1.
+
+    Wanneer een gebruiker de waarde van een input field met class 'quantity' aanpast, moet de totaal prijs van het product berekend worden en worden weergeven in op het scherm.
+
+
+    *** HINTS ***
+        1. Begin met document en gebruik een methode om een element te selecteren (in dit geval een input field).
+        2. Gebruik een event listener die de waarde die de waarde van het input field registreert.
+        3. Om het juiste object te updaten maak gebruik van DOM traversion. => https://javascript.info/dom-navigation 
+    ***    ***
+    
+    - Opdracht 2.
+
+    Na dat de prijs per product is berekend, bereken de totaal prijs van alle producten en laat de waarde zien in de span met id 'total-price'. 
+
+    - Opdracht 3.
+
+    Maak een nieuwe sectie in html waar in je een product kan toevoegen aan het bestaande assortiment.
+
+    Dus twee input fields: één voor de naam van het product, één voor de prijs van het product.
+
+*/
